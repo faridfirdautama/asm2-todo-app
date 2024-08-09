514ac0de3c62fed07e7767a1d946634aafa8c0a6
Dadit <dadit.utama@gmail.com>
2024-08-09 10:13:06 +0700

    feat: setup routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./dB/db.connect";
+import { todoRouter } from "./routes/todo.route";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 connectDB();
+
+// Routes
+app.use("/api/v1/todos", todoRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
