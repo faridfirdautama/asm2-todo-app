2b0acf61ec5e1fb7e7427dec3bfebae8a89d9d88
Dadit <dadit.utama@gmail.com>
2024-08-09 09:14:14 +0700

    feat: setup db connection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./dB/db.connect";
 
 dotenv.config();
 const app = express();
+connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
