faeceaa9340c3053b2695d7e53ed4fa857c002e7
Dadit <dadit.utama@gmail.com>
2024-08-09 22:43:57 +0700

    feat: define user login entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,12 @@
-export interface IUser {
+interface IUser {
   name: string;
   email: string;
   password: string;
 }
+
+interface IUserLoginRequest {
+  email: string;
+  password: string;
+}
+
+export { IUser, IUserLoginRequest };
