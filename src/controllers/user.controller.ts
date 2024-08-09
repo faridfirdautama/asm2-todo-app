dd663c74b030d08fe4b98b6eaf19312af7a09546
Dadit <dadit.utama@gmail.com>
2024-08-09 22:45:00 +0700

    feat: adding handle login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,6 +13,26 @@ const UserController = {
       .status(201)
       .json({ message: "User successfully created", data: { name, email } });
   },
+  handleLogin: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const messages = await UserService.userLogin({
+      email,
+      password,
+    });
+    if (typeof messages === "string") {
+      return res.status(400).json({ messages });
+    }
+
+    return res
+      .cookie("accessToken", messages?.accessToken, {
+        httpOnly: true,
+      })
+      .cookie("refreshToken", messages?.refreshToken, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ message: "You are successfully logged in" });
+  },
 };
 
 export default UserController;
