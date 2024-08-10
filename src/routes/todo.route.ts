de669692f7f8e2b220f91427f830b190562b4f22
Dadit <dadit.utama@gmail.com>
2024-08-10 09:40:59 +0700

    feat: implement middleware in todo routes

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import TodoController from "../controllers/todo.controller";
+import { checkAuth } from "../middleware/auth.middleware";
 
 export const todoRouter = express.Router();
+todoRouter.use(checkAuth);
 
 todoRouter.get("/", TodoController.handleGetAllTodos);
 todoRouter.get("/:id", TodoController.handleGetTodo);
